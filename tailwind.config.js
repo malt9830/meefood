e349d9cf8949d587d0d016f25916344737aa69f4
malt9830 <malt9830@stud.kea.dk>
2022-05-02 23:23:00 +0200

    Installed font
    - Installed Google font 'Inter'
    - Set default font to 'Inter'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter, ui-sans-serif, system-ui"],
+    },
     extend: {},
   },
   plugins: [],
