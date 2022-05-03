65a1e7f8b6092f8447fb9403fec2f8aaf947e256
malt9830 <malt9830@stud.kea.dk>
2022-05-03 19:12:35 +0200

    Added axios to single page
    - axios to get test restaurant on single page

--- src/views/RestaurantPage.vue
+++ src/views/RestaurantPage.vue
@@ -1,15 +1,24 @@
 <template>
   <main>
-    <h1>{{ restaurantInfo }}</h1>
+    <h1 v-if="restaurant">{{ restaurant.name }}</h1>
   </main>
 </template>
 
 <script setup>
 const route = useRoute()
 
-const restaurantInfo = ref(0)
+const restaurant = ref()
 
 onMounted(() => {
-  restaurantInfo.value = route.params.id
+  axios.get(`https://meefood-1d4e.restdb.io/rest/restaurants/${route.params.id}`, {
+    headers: {
+      "Content-Type": "application/json",
+      "x-apikey": "62714176fcf9897eb111a104",
+      "cache-control": "no-cache",
+    }
+  })
+  .then(res => {
+    restaurant.value = res.data
+  })
 })
 </script>
