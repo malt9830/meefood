57b7d60d48a055aef42902ecec9f57cca4128a1a
malt9830 <malt9830@stud.kea.dk>
2022-05-04 18:46:04 +0200

    Started restaurant list cards + added some icons
    - Started making the restaurant cards for the list page
    - Added icons for smileys, delivery cost and minimum cost
    - Also added testing images for restaurants

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -1,10 +1,31 @@
 <template>
-  <main class="max-w-5xl mx-auto p-16 px-4">
-    <h2>Restaurants</h2>
-    <div class="grid grid-cols-3">
-      <article v-for="rest in restaurants" :key="rest._id">
-        <h1 :style="`background-color:${rest.primaryColor}; color:${rest.secondaryColor}`">{{ rest.name }}</h1>
-      </article>
+  <main class="max-w-7xl mx-auto pt-24 px-2">
+    <h2 class="text-2xl font-semibold">Restauranter</h2>
+    <div class="grid gap-4 pt-4 sm:grid-cols-2 md:grid-cols-3">
+        <a class="block h-56 cursor-pointer rounded-2xl group transform hover:scale-[103%] duration-200" v-for="rest in restaurants" :key="rest._id">
+          <div class="grid content-center w-full h-36 rounded-t-2xl overflow-hidden">
+            <img class="w-full h-auto transform scale-105 group-hover:scale-110 duration-200" src="@/src/assets/test-images/splash.png">
+          </div>
+          <div class="px-4 py-2">
+            <h1>{{ rest.name }} - {{ rest.slogan }}</h1>
+            <hr class="my-1">
+            <p class="flex gap-4 text-md">
+              <span>{{ rest.tags }}</span>
+              <span class="flex gap-1">
+                <img src="@/src/assets/icons/smile1.svg">
+                <span>{{ rest.rating }}</span>
+              </span>
+              <span class="flex gap-1">
+                <img src="@/src/assets/icons/bike.svg">
+                <span>{{ rest.deliveryCost }} DKK</span>
+              </span>
+              <span class="flex gap-1">
+                <img src="@/src/assets/icons/bag.svg">
+                <span>Min. {{ rest.minimumPrice }}</span>
+              </span>
+            </p>
+          </div>
+        </a>
     </div>
   </main>
 </template>
@@ -25,3 +46,9 @@ onMounted(() => {
   })
 })
 </script>
+
+<style scoped>
+a {
+  box-shadow: 0px 1px 5px #707070;
+}
+</style>
