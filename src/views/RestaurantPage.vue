eb2f2f82922056e2a07322caa21854820b1d05eb
malt9830 <malt9830@stud.kea.dk>
2022-05-05 17:27:17 +0200

    Replaced restdb with Airtable
    - Removed code using axios to get restDB data
    - Installed airtable.js module
    - Added code on restaurant list page to get all restaurants using Airtable API
    - Added code on restaurant single page to get specific restaurant based on slug using Airtable API

--- src/views/RestaurantPage.vue
+++ src/views/RestaurantPage.vue
@@ -1,24 +1,40 @@
 <template>
   <main>
-    <h1 v-if="restaurant">{{ restaurant.name }}</h1>
+    <div v-if="!loaded" class="h-[70vh] w-full bg-[url('./assets/splash-image.png')] bg-center bg-cover bg-fixed"></div>
+    <div v-if="loaded" :style="`background-image: url(${ restaurant.splash[0].url })`" class="h-[70vh] w-full bg-[url('./assets/splash-image.png')] bg-center bg-cover bg-fixed"></div>
+    <h1 v-if="loaded" class="text-3xl font-semibold p-12">{{ restaurant.name }}</h1>
   </main>
 </template>
 
 <script setup>
+import Airtable from 'airtable'
+
 const route = useRoute()
+
+const loaded = ref(false)
 
 const restaurant = ref()
 
-onMounted(() => {
-  axios.get(`https://meefood-1d4e.restdb.io/rest/restaurants?q={"slug":"${route.params.id}"}`, {
-    headers: {
-      "Content-Type": "application/json",
-      "x-apikey": "62714176fcf9897eb111a104",
-      "cache-control": "no-cache",
-    }
-  })
-  .then(res => {
-    restaurant.value = res.data[0]
-  })
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: 'keyiA3gmotxYIJjLc'
+})
+
+var base = Airtable.base('appRiP6vD6l7EmCYB')
+
+// --- Use find method to immediately get correct record but requires id instead of slug
+// base('restaurants').find('recCXad6ewDgxkuAf', (err, record) => {
+//   if (err) {console.log(err); return}
+//   restaurant.value = record.fields
+//   loaded.value = true
+// })
+
+// --- Use select method with a filter to get specific restaurant from slug
+base('restaurants').select({
+  filterByFormula: `{slug} = '${route.params.id}'`
+}).eachPage(res => {
+  restaurant.value = res[0].fields
+
+  loaded.value = true
 })
 </script>
--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -2,15 +2,15 @@
   <main class="max-w-7xl mx-auto pt-24 px-2">
     <h2 class="text-2xl font-semibold">Restauranter</h2>
     <div class="grid gap-4 pt-4 sm:grid-cols-2 md:grid-cols-3">
-        <a class="block h-56 cursor-pointer rounded-2xl group transform hover:scale-[103%] duration-200" v-for="rest in restaurants" :key="rest._id">
+        <RouterLink class="block cursor-pointer rounded-2xl group transform hover:scale-[103%] duration-200" :to="`/restaurant/${rest.slug}`" v-for="rest in restaurants" :key="rest.slug">
           <div class="grid content-center w-full h-36 rounded-t-2xl overflow-hidden">
-            <img class="w-full h-auto transform scale-105 group-hover:scale-110 duration-200" src="@/src/assets/test-images/splash.png">
+            <img class="w-full h-auto transform scale-105 group-hover:scale-110 duration-200" :src="rest.splash[0].url">
           </div>
           <div class="px-4 py-2">
             <h1>{{ rest.name }} - {{ rest.slogan }}</h1>
             <hr class="my-1">
-            <p class="flex gap-4 text-sm">
-              <span>{{ rest.tags }}</span>
+            <p class="flex flex-wrap gap-4 text-sm">
+              <span class="capitalize">{{ rest.tags[0] }}</span>
               <span class="flex gap-1 items-center">
                 <img class="h-4" src="@/src/assets/icons/smile1.svg">
                 <span>{{ rest.rating }}</span>
@@ -25,24 +25,26 @@
               </span>
             </p>
           </div>
-        </a>
+        </RouterLink>
     </div>
   </main>
 </template>
 
 <script setup>
+import Airtable from 'airtable'
+
 const restaurants = ref([])
 
-onMounted(() => {
-  axios.get('https://meefood-1d4e.restdb.io/rest/restaurants', {
-    headers: {
-      "Content-Type": "application/json",
-      "x-apikey": "62714176fcf9897eb111a104",
-      "cache-control": "no-cache",
-    }
-  })
-  .then(res => {
-    restaurants.value = res.data
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: 'keyiA3gmotxYIJjLc'
+})
+
+var base = Airtable.base('appRiP6vD6l7EmCYB')
+
+base('restaurants').select().eachPage(res => {
+  res.forEach(rest => {
+    restaurants.value.push(rest.fields)
     console.log(restaurants.value)
   })
 })
