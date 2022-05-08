8f85f36365a25540ac2b9ee375ec27faf8b3967a
malt9830 <malt9830@stud.kea.dk>
2022-05-08 16:50:26 +0200

    Added delivery time
    - Added delivery time + time icon svg
    - Updated logo padding to match new scale of logos

--- src/components/restaurant-list/RestaurantCard.vue
+++ src/components/restaurant-list/RestaurantCard.vue
@@ -5,11 +5,18 @@
         <img class="absolute w-full h-auto place-self-center object-cover -z-10 transform scale-110 group-hover:scale-100 duration-200" :src="restaurant.splash[0].url">
         <div class="h-[120%] w-full bg-gradient-to-t from-black via-transparent" />
       </div>
-      <div class="flex flex-row mt-auto pl-2 transform translate-y-6">
+      <div class="flex flex-row mt-auto px-2 transform translate-y-6">
         <div class="grid w-20 aspect-square bg-white border border-emerald-500 rounded-full overflow-hidden">
-          <img class="w-full place-self-center" :src="restaurant.logo[0].url">
+          <img class="w-full min-w-full place-self-center p-2" :src="restaurant.logo[0].url">
         </div>
-        <p class="text-2xl font-semibold text-white px-2 pb-2 self-center">{{ restaurant.name }}</p>
+        <p class="text-2xl font-semibold text-white whitespace-nowrap px-2 pb-2 self-center">{{ restaurant.name }}</p>
+        <div class="flex bg-white border border-emerald-500 h-10 p-1 pl-3 ml-auto rounded-full transform translate-y-9">
+          <div class="mr-2 place-self-center text-emerald-500 text-center">
+            <p class="text-sm leading-none font-bold">{{ restaurant.deliveryTime }}</p>
+            <p class="text-xs leading-none font-semibold">min</p>
+          </div>
+          <img class="h-full w-auto" src="@/src/assets/icons/time.svg">
+        </div>
       </div>
     </div>
     <div class="px-4 pb-2 pt-1">
