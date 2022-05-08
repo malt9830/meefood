441933d19ee757187a2d37f798d8633d5183cd40
malt9830 <malt9830@stud.kea.dk>
2022-05-08 17:12:54 +0200

    Added initial Filter menu
    - Added FilterModal component via <Teleport>

---
+++ src/components/restaurant-list/FilterModal.vue
@@ -0,0 +1,15 @@
+<template>
+  <Teleport to="body">
+    <div class="fixed left-0 top-0 z-20 h-full w-full flex justify-end">
+      <div @click="$emit('closeFilter')" class="absolute left-0 top-0 h-full w-full bg-black opacity-50" />
+      <aside class="h-full w-96 max-w-full z-10 bg-white px-8 shadow-2xl">
+        <div class="mt-20">
+          <p class="text-2xl text-gray-500 font-semibold mt-8">Filtrering</p>
+          <p></p>
+          <p class="text-2xl text-gray-500 font-semibold mt-8">Sortering</p>
+          <p></p>
+        </div>
+      </aside>
+    </div>
+  </Teleport>
+</template>
--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -2,16 +2,19 @@
   <main class="max-w-7xl mx-auto pt-24 px-4">
     <div class="flex flex-row justify-between">
       <h2 class="text-3xl font-semibold">Restauranter</h2>
-      <img class="w-9 cursor-pointer transform hover:scale-105 duration-200" src="@/src/assets/icons/filter.svg">
+      <img @click="showFilter = !showFilter" class="w-9 cursor-pointer transform hover:scale-105 duration-200" src="@/src/assets/icons/filter.svg">
     </div>
     <div v-if="loaded" class="grid gap-6 pt-4 md:grid-cols-2 lg:grid-cols-3">
       <RestaurantCard v-for="rest in restaurants" :key="rest.slug" :restaurant="rest"/>
     </div>
+    <FilterModal v-if="showFilter" @closeFilter="showFilter = false"/>
   </main>
 </template>
 
 <script setup>
 import Airtable from 'airtable'
+
+const showFilter = ref(false)
 
 const restaurants = ref([])
 
