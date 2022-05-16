c2e4c82227bb6ae975d15d5f9e8a042551344ac7
Maiich7 <mettemaija@hotmail.com>
2022-05-16 17:30:04 +0200

    update footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,18 +1,56 @@
 <template>
-  <footer class="bg-emerald-800 text-white mt-20 py-12">
-    <div class="max-w-6xl mx-auto px-4 flex justify-between">
-      <div>
-        <p class="text-2xl font-bold uppercase">Support</p>
-      </div>
-      <div>
-        <p class="text-2xl font-bold uppercase">Generalt</p>
-      </div>
-      <div>
-        <p class="text-2xl font-bold uppercase">Meefood</p>
-      </div>
-    </div>
-  </footer>
+	<footer class="bg-emerald-900 py-6 lg:py-10 mt-40">
+		<div class="py-8 mx-auto max-w-7xl px-6 md:mx-8 lg:mx-auto">
+			<div class="grid lg:grid-cols-3">
+				<div class="text-white self-center pb-6 lg:pb-6 place-self-center">
+					<a href="http://">
+						<img class="h-10 w-auto" src="src/assets/logos/meefood-logo-white.svg" alt="" />
+					</a>
+				</div>
+				<div class="flex space-x-4 justify-center md:justify-start pb-12">
+					<a href="http://">
+						<svg class="h-10 w-auto" xmlns="http://www.w3.org/2000/svg" width="56.382" height="56.382" viewBox="0 0 56.382 56.382">
+							<path id="Icon_awesome-facebook-square" data-name="Icon awesome-facebook-square" d="M50.341,2.25H6.041A6.041,6.041,0,0,0,0,8.291v44.3a6.041,6.041,0,0,0,6.041,6.041H23.314V39.463H15.385V30.441h7.929V23.564c0-7.822,4.657-12.142,11.789-12.142a48.033,48.033,0,0,1,6.987.609v7.677H38.154c-3.877,0-5.087,2.406-5.087,4.874v5.858h8.656l-1.384,9.022H33.068V58.632H50.341a6.041,6.041,0,0,0,6.041-6.041V8.291A6.041,6.041,0,0,0,50.341,2.25Z" transform="translate(0 -2.25)" fill="#fff" />
+						</svg>
+					</a>
+					<a href="http://">
+						<svg class="h-10 w-auto" xmlns="http://www.w3.org/2000/svg" width="56.394" height="56.381" viewBox="0 0 56.394 56.381">
+							<path
+								id="Icon_awesome-instagram"
+								data-name="Icon awesome-instagram"
+								d="M28.2,15.973A14.455,14.455,0,1,0,42.653,30.428,14.433,14.433,0,0,0,28.2,15.973Zm0,23.853a9.4,9.4,0,1,1,9.4-9.4,9.415,9.415,0,0,1-9.4,9.4ZM46.616,15.382a3.372,3.372,0,1,1-3.372-3.372A3.364,3.364,0,0,1,46.616,15.382ZM56.19,18.8c-.214-4.517-1.246-8.517-4.554-11.813s-7.3-4.328-11.813-4.554c-4.655-.264-18.607-.264-23.262,0-4.5.214-8.5,1.246-11.813,4.542S.419,14.274.193,18.791c-.264,4.655-.264,18.607,0,23.262C.407,46.57,1.438,50.57,4.747,53.866s7.3,4.328,11.813,4.554c4.655.264,18.607.264,23.262,0,4.517-.214,8.517-1.246,11.813-4.554s4.328-7.3,4.554-11.813c.264-4.655.264-18.595,0-23.249ZM50.177,47.048a9.515,9.515,0,0,1-5.359,5.359C41.106,53.879,32.3,53.539,28.2,53.539s-12.921.327-16.619-1.132a9.515,9.515,0,0,1-5.359-5.359C4.747,43.336,5.087,34.53,5.087,30.428S4.76,17.508,6.219,13.809a9.515,9.515,0,0,1,5.359-5.359C15.29,6.978,24.1,7.317,28.2,7.317S41.118,6.99,44.817,8.449a9.515,9.515,0,0,1,5.359,5.359c1.472,3.711,1.132,12.518,1.132,16.619S51.649,43.349,50.177,47.048Z"
+								transform="translate(0.005 -2.238)"
+								fill="#fff"
+							/>
+						</svg>
+					</a>
+					<a href="http://">
+						<svg class="h-10 w-auto" xmlns="http://www.w3.org/2000/svg" width="56.382" height="56.382" viewBox="0 0 56.382 56.382">
+							<path id="Icon_awesome-linkedin" data-name="Icon awesome-linkedin" d="M52.354,2.25H4.015A4.045,4.045,0,0,0,0,6.315V54.567a4.045,4.045,0,0,0,4.015,4.065h48.34a4.055,4.055,0,0,0,4.027-4.065V6.315A4.055,4.055,0,0,0,52.354,2.25ZM17.04,50.577H8.684V23.67h8.369V50.577ZM12.862,20a4.845,4.845,0,1,1,4.845-4.845A4.847,4.847,0,0,1,12.862,20Zm35.5,30.582H40.008V37.489c0-3.121-.063-7.136-4.342-7.136-4.354,0-5.021,3.4-5.021,6.909V50.577H22.288V23.67h8.017v3.675h.113A8.8,8.8,0,0,1,38.334,23c8.457,0,10.03,5.575,10.03,12.824Z" transform="translate(0 -2.25)" fill="#fff" />
+						</svg>
+					</a>
+				</div>
+
+				<div class="text-white">
+					<div class="pb-6">
+						<h5 class="font-bold">Telefon</h5>
+						<p>71 99 19 01</p>
+						<p>Man - fredag 09:00 - 16:00</p>
+					</div>
+
+					<h5 class="font-bold">Email</h5>
+					<p class="pb-6">Kontakt@meeshop.dk</p>
+				</div>
+				<div class="text-white">
+					<div class="pb-6">
+						<h5 class="font-bold">Adresse</h5>
+						<p>Marielundsvej 17F</p>
+						<p>2730 Herlev</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
