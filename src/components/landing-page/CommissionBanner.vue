4e4a9de7527b2be3f06c2c5c4a29e393685f64ea
malt9830 <malt9830@stud.kea.dk>
2022-06-02 19:12:07 +0200

    Added new transparency logo

--- src/components/landing-page/CommissionBanner.vue
+++ src/components/landing-page/CommissionBanner.vue
@@ -1,8 +1,10 @@
 <template>
 	<section class="bg-emerald-700 py-10 px-4 text-white">
-    <div class="max-w-4xl mx-auto grid gap-2 sm:grid-cols-2">
-      <div class="cursor-zoom-in"></div>
-      <div class="text-lg">
+    <div class="max-w-4xl mx-auto grid gap-8 sm:grid-cols-[2fr_3fr]">
+      <div class="flex flex-col justify-center px-12 sm:p-0">
+        <img src="../../assets/commission/transparency-logo.svg"/>
+      </div>
+      <div class="flex flex-col justify-center text-lg">
         <h2 class="text-3xl font-semibold">Vi skjuler intet</h2>
         <p class="mt-2">I Meefood er kommissionen kun 2% og altid 2% - om du bestiller gennem os eller via restauranten selv.</p>
         <p class="mt-2">Vi prøver ikke at konkurrere med vores restaurant-partnere. Vores success er deres success, og vi prøver i stedet at vækste vores forretninger sammen.</p>
